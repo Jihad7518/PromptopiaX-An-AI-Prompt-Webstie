07c0f1a41d5989219aa089aafa7d4e5c466813b5
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-22 01:54:22 +0600

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,1 +1,20 @@
 
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
+const Nav = () => {
+  const { data: session } = useSession();
+
+  const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  }, []);
