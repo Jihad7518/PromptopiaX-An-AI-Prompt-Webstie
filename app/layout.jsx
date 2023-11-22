6dbf8f6079fb37932c7add26484a33805d29c88e
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-23 00:21:01 +0600

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,29 @@
 
+import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+  title: "PromptopiaX",
+  description: "Unleash Limitless Creativity with AI-Powered Prompts!"
+};
+
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
