d56ae92ba1e0a342c28378f5bd137ca62603af5b
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-23 00:12:01 +0600

    Update Form.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,1 +1,29 @@
+import Link from "next/link";
 
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+  return (
+    <section className='w-full max-w-full flex-start flex-col'>
+      <h1 className='head_text text-left'>
+        <span className='blue_gradient'>{type} Post</span>
+      </h1>
+      <p className='desc text-left max-w-md'>
+        {type} Unleash a World of Inspiration: Share Incredible Prompts Globally and Set Your Imagination Free on Any AI-Powered Platform!
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
+      >
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Your AI Prompt
+          </span>
+
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder='Write your post here'
+            required
+            className='form_textarea '
+          />
+        </label>
